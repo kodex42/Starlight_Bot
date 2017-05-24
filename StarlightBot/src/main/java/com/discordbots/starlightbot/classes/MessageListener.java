97767937ef7da9d248d945b0d0fe174baf9328c0
Matt Buchanan <mbkodex42@hotmail.com>
2017-05-24 12:57:20 -0400

    LogBot

--- StarlightBot/src/main/java/com/discordbots/starlightbot/classes/MessageListener.java
+++ StarlightBot/src/main/java/com/discordbots/starlightbot/classes/MessageListener.java
@@ -8,7 +8,6 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -17,6 +16,8 @@ import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -26,7 +27,7 @@ import java.util.Objects;
 public class MessageListener extends ListenerAdapter {
     private static JDA jda;
     private static HashMap<String, Command> commands = new HashMap<>();
-    private static HashMap<String, FileOutputStream> channelStreams = new HashMap<>();
+    private static HashMap<String, File> channelStreams = new HashMap<>();
     private static final CommandParser parser = new CommandParser();
 
     public static void main(String[] args)
@@ -36,11 +37,7 @@ public class MessageListener extends ListenerAdapter {
         jda.setAutoReconnect(true);
 
         for (TextChannel channel : jda.getTextChannels()) {
-            try {
-                channelStreams.put(channel.getName(), new FileOutputStream(new File("A:\\CHAT LOGS\\" + channel.getName())));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+            channelStreams.put(channel.getName(), new File("A:\\CHAT LOGS\\" + channel.getName() + ".txt"));
         }
 
         commands.put("ping", new PingCommand());
@@ -50,6 +47,13 @@ public class MessageListener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getMessage().getContent().startsWith("-") && !Objects.equals(event.getMessage().getAuthor().getId(), event.getJDA().getSelfUser().getId()))
             Parser.parse(parser.parse(event.getMessage().getContent(), event));
+        else {
+            try {
+                Parser.log(event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         if (!event.isFromType(ChannelType.PRIVATE)) {
             System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
@@ -71,9 +75,23 @@ public class MessageListener extends ListenerAdapter {
                     commands.get(cmd.invoke).executed(false, cmd.event);
                 }
             }
-            else {
+        }
 
+        private static void log(MessageReceivedEvent event) throws IOException {
+            String messageLog = "[" + new Date().toString() + "]:[" + event.getChannel().getName() + "]:[" + event.getAuthor().getName() + "]:" + event.getMessage().getContent() + "\n";
+            FileOutputStream fout;
+            File file;
+
+            if (channelStreams.containsKey(event.getChannel().getName())) {
+                fout = new FileOutputStream(channelStreams.get(event.getChannel().getName()), true);
+            } else {
+                file = new File("A:\\CHAT LOGS\\" + event.getChannel().getName() + ".txt");
+                fout = new FileOutputStream(file, true);
+                channelStreams.put(event.getChannel().getName(), file);
             }
+
+            fout.write(messageLog.getBytes());
+            fout.close();
         }
     }
 }
